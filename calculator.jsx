f65a037f9ad4d0e53da58d36988e3c81abf95ec4
Jennifer Georgevich <jenngeorge@berkeley.edu>
2017-10-01 11:51:44 -0400

    First commit: react + webpack setup

---
+++ calculator.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", function(){
+  ReactDOM.render(<div>I'm the react calculator </div>, document.getElementById('root'));
+});
